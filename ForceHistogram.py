85d6e9537a0a35c636cfa2597f9c06bcaa550f59
tinkersaur <tinkersaur@gmail.com>
2017-09-28 22:43:13 -0600

    change variables in ForceHistogram to merge with OccupGrd.py.

--- ForceHistogram.py
+++ ForceHistogram.py
@@ -4,28 +4,27 @@ class ForceHistogram:
 
     def __init__(self):
         self.setHistParam(72)
-        self.gdir=0
+        self.goal_dir=0
 
     # Set the number of elemts in the histogram
-    def setHistParam(self, na):
-        self.na=na
-        self.hist=[0.0 for x in range(self.na) ]
+    def setHistParam(self, nAngles):
+        self.nAngles=nAngles
+        self.hist=[0.0 for a in range(self.nAngles) ]
 
 
-    def setGridParam(self, dx, dy, x0, y0):
-        self.dx=dx
-        self.dy=dy
+    def setGridParam(self, resoultion, x0, y0):
+        self.resolution=resolution
         self.x0=x0
         self.y0=y0
 
-    def setRobotPos(x, y):
-        self.x=x
-        self.y=y
+    def setRobotPos(xPos, yPos):
+        self.xPos=xPos
+        self.yPos=yPos
 
     #
 
     def setGoalDirection(angle):
-        self.gdir
+        self.goal_dir=angle
 
 
     def setGrid(self, grid):
@@ -33,22 +32,23 @@ class ForceHistogram:
 
     # angle in radians to index of the histogram:
     def angle2index(self, a):
-        return int((a/pi+1)/2*self.na+0.5) % self.na 
+        return int((a/pi+1)/2*self.nAngles+0.5) % self.nAngles 
 
     def index2angle(self, k):
-        return ((k*1.0)/self.na-0.5)*2*pi
+        return ((k*1.0)/self.nAngles-0.5)*2*pi
 
-    # return [Speed, TurnAngle]
-    def navigate:
+    # Take the occupancy grid, and calculate
+    # self.speed, and self.turn_angle
+    def calcForceHist:
 
         # precalculate some constants:
-        ny=len(grid)
-        nx=len(grid[0])
+        nRows=len(grid)
+        nCols=len(grid[0])
         w0=1.0
         // maximum distance to the obstacle.
-        d_max=hypot(self.dx*nx/2, self.dy*ny)
+        d_max=hypot(self.resolution*nCols/2, self.resolution*nRows)
         dw=1.0
-        da = 2*pi / self.na
+        da = 2*pi / self.nAngles
     
         # The treshold in the histogram for finding valleys
         # The value of 0.1 is a total guess.
@@ -68,13 +68,13 @@ class ForceHistogram:
         # a -- angle of the obtacle.
         # w -- the weight of the obstacle.
         # relative to the robot.
-        for j in range(ny):
-            for i in range(nx):
-                rx = grid[i][j][0] - x
-                ry = grid[i][j][1] - y
+        for j in range(nRows):
+            for i in range(nCols):
+                rx = grid[i][j][0] - xPos
+                ry = grid[i][j][1] - yPos
 
-                a=atan2(y,x)
-                d=hypot(x,y)
+                a=atan2(ry,rx)
+                d=hypot(rx,ry)
 
                 k=self.angle2index(a)
 
@@ -89,12 +89,12 @@ class ForceHistogram:
         # This array contains the array of the start of the
         # valley and its width.
         valleys = []
-        for i in range(self.na):
+        for i in range(self.nAngles):
             if self.hist[i]>Fmax:
                 continue
 
             n=1
-            while self.hist[ (i+j) % self.na ] > Fmax:
+            while self.hist[ (i+j) % self.nAngles ] > Fmax:
                 n+=1
 
             if n>=Lmin:
@@ -107,7 +107,7 @@ class ForceHistogram:
         # then go in the middle of it.
 
         found = False
-        k = (self.gdir/pi+1)/2*self.na
+        k = (self.goal_dir/pi+1)/2*self.nAngles
         for v in valleys:
             if k>=v[0] && k<=v[0]+v[1]-1:
                 found = True
@@ -125,13 +125,13 @@ class ForceHistogram:
                 if (!found):
                     TurnAngle = edge
                     found = True
-                elif fabs(TurnAngle-self.gdir) < \
-                     fabs(edge-self.gdir):
+                elif fabs(TurnAngle-self.goal_dir) < \
+                     fabs(edge-self.goal_dir):
                     TurnAngle = edge
 
                 edge = self.index2angle(v[0]+v[1]) 
-                elif fabs(TurnAngle-self.gdir) < \
-                     fabs(edge-self.gdir):
+                elif fabs(TurnAngle-self.goal_dir) < \
+                     fabs(edge-self.goal_dir):
                     TurnAngle = edge
 
         Vmax=1.0
@@ -144,5 +144,6 @@ class ForceHistogram:
 
         Speed = (Vmax-Vmin)*(1.0 - ObstacleSize) + Vmin
 
-        return [Speed, TurnAngle]
+        self.speed = Speed
+        self.turn_angle = TurnAngle
 
